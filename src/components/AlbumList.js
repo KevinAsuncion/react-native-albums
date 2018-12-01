db1d8eea412c9595bf5ede637604ac5c84fc6881
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-12-01 09:08:55 -0800

    added album detail

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import AlbumDetail from './AlbumDetail';
 import axios from 'axios';
 
 class AlbumList extends Component {
@@ -12,10 +13,13 @@ class AlbumList extends Component {
           albums: res.data
       });
   }  
+  renderAlbums() {
+    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+  }
   render() {
     return (
       <View>
-        {this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>)}
+        {this.renderAlbums()}
       </View>
     );
   }
