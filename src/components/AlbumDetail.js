5732ace4f0d825516e5c8a3c93606f364537c8ea
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-12-01 18:26:55 -0800

    add Card

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { View, Text } from "react-native";
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = props => {
-  return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
-  );
+  return <Card>
+      <CardSection>
+        <View />
+        <View>
+          <Text>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
+      </CardSection>
+    </Card>;
 };
+
+const styles = {
+    headerContentStyle: {
+        
+    }
+}
 
 export default AlbumDetail;
