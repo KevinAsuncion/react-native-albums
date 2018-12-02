e6239e6177b32b304ae19faa9a8c85c295317f1f
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-12-01 19:16:35 -0800

    add button

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { Text, ScrollView } from "react-native";
 import AlbumDetail from './AlbumDetail';
 import axios from 'axios';
 
@@ -18,9 +18,9 @@ class AlbumList extends Component {
   }
   render() {
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
